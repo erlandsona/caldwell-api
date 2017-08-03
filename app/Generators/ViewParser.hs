c7e0b44d5e2c174b4a01f71ee5fa2d5a3741d441
Austin Erlandson <austin@erlandson.com>
2017-08-02 21:58:49 -0500

    I've got a nice Tree and the nodes matching data-care!

--- app/Generators/ViewParser.hs
+++ app/Generators/ViewParser.hs
@@ -2,17 +2,15 @@ module Generators.ViewParser where
 
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Match
+import Text.HTML.TagSoup.Tree
 
 
 
 main :: IO ()
 main = do
     document <- readFile "app/Server/TestView/snippet.html" 
-    print $ filter (\tag ->
-                        if isTagOpen tag
-                        then anyAttr "<li data-care>"
-                        else tag
-                    ) $ parseTags document
+    let attributes = [x | x@(TagBranch _ [("data-care", _)] _) <- universeTree (parseTree document) ]
+    print attributes
 
     -- print $ render ast "Stuff"
 
